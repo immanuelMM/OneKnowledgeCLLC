d70fd6a95dbb7ce8be0bacf3292ccdec09fe617f
immanuelMM <merino.mahusay@gmail.com>
2022-02-20 11:29:52 +0800

    slove problem in close button modal

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -16,46 +16,57 @@
 //     })
 // })
 
-function sendEmail(){
-  Swal.fire({ 
+function sendEmail() {
+  Swal.fire({
     title: "Sending .. . ",
     showConfirmButton: false,
     onBeforeOpen: () => {
-              Swal.showLoading()
-              },
+      Swal.showLoading();
+    },
   });
   const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 5000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-        Email.send({
-        Host : "smtp.gmail.com",
-        Username : "mawe.merino@gmail.com",
-        Password : "ncikvxpedlryvdlx",
-        To : "info@oneknowledgeconsulting.com",
-        From : document.getElementById("email").value,
-        Subject : "New Contact form Enguiry",
-        Body : "Name: " 
-                              + document.getElementById("name").value 
-            +  "<br> Email: " + document.getElementById("email").value
-            +  "<br> Subject: " + document.getElementById("subject").value
-            +  "<br> Message: " + document.getElementById("message").value,
-           
-        }).then(
-          message =>  
-        
-
-          Toast.fire({
-            icon: 'success',
-            title: 'Message successfully Sent'
-          })
-            
-        );
-    }
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "mawe.merino@gmail.com",
+    Password: "ncikvxpedlryvdlx",
+    To: "info@oneknowledgeconsulting.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact form Enguiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Subject: " +
+      document.getElementById("subject").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) =>
+    Toast.fire({
+      icon: "success",
+      title: "Message successfully Sent",
+    })
+  );
+}
+$(document).ready(function () {
+  setTimeout(function () {
+    $("#myModal").modal("show");
+  }, 3000);
+  $("#myModal").modal("hide");
+});
+function closeModal() {
+  $("#myModal").modal("hide");
+}
+$("#close").click(function () {
+  closeModal();
+});
